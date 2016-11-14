d7098dca585c69edb6e943168e2539247980b889
luxuanren <867773467@qq.com>
2016-11-15 00:59:49 +0800

    springmvc-archetype 0.0.1 SNAPSHOT

---
+++ src/main/java/com/bookstore/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home.do")
+	public String homePage() {
+		return "home";
+	}
+}
